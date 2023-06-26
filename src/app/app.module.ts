8ab0922d6a06c91d2f7534cc7369b2b74dbe8fee
hannes.schniz <hannes.schniz@gmx.de>
2023-06-26 22:07:06 +0200

    added dots and fixed height

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatCardModule} from '@angular/material/card';
-import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { AppNavComponent } from './app-nav/app-nav.component';
 import { MatListModule } from '@angular/material/list';
-import { SlidepanelComponent } from './slidepanel/slidepanel.component'; 
-import {MatProgressBarModule} from '@angular/material/progress-bar'; 
+import { SlidepanelComponent } from './slidepanel/slidepanel.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
