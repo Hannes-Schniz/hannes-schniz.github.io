763297661efc349f95391b2bdc486a3116853dd3
hannes.schniz <hannes.schniz@gmx.de>
2023-07-05 18:24:14 +0200

    fixed animation

--- src/app/slidepanel/slidepanel.component.ts
+++ src/app/slidepanel/slidepanel.component.ts
@@ -18,7 +18,7 @@ const left = [
 ];
 
 const right = [
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  query(':enter, :leave', style({ position: 'absolute', width: '100%' }), { optional: true }),
   group([
     query(':enter', [style({ transform: 'translateX(100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
       optional: true,
