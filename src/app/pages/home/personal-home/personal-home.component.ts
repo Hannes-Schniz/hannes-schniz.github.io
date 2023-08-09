b334c7f33fc624e69651a8ab9fc2dc199f5292e2
TheXample <83277529+TheXample@users.noreply.github.com>
2023-08-09 14:23:23 +0200

    Update personal-home.component.ts

--- src/app/pages/home/personal-home/personal-home.component.ts
+++ src/app/pages/home/personal-home/personal-home.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { localstorageHelper } from '../../../shared/helper/localstorage.helper';
-import { TEST } from '../../../shared/constants/localstorage.constants';
-import { LocalstorageModel } from '../../../shared/models/localstorage.model';
 
 @Component({
   selector: 'app-personal-home',
@@ -9,31 +6,7 @@ import { LocalstorageModel } from '../../../shared/models/localstorage.model';
   styleUrls: ['./personal-home.component.scss'],
 })
 export class PersonalHomeComponent {
-  public storage: localstorageHelper;
 
   constructor() {
-    this.storage = localstorageHelper.register(TEST);
-  }
-
-  localstorageADD() {
-    this.storage.add(
-      new LocalstorageModel('test', 'test'),
-      new LocalstorageModel('test2', 'test2')
-    );
-  }
-  localstorageFORCE() {
-    this.storage.force(
-      new LocalstorageModel('test', 'this is forced'),
-      new LocalstorageModel('test2', 'thisisforced')
-    );
-  }
-  localstorageUPDATE() {
-    this.storage.update(
-      new LocalstorageModel('test', 'this is updated'),
-      new LocalstorageModel('test2', 'this is updated')
-    );
-  }
-  localstorageDELETE() {
-    this.storage.delete('test', 'test2');
   }
 }
