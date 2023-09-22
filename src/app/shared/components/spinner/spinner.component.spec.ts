bff3fe91e7fb34850d1e53298e4d90116b01d08d
Hannes <contact@hannesschniz.de>
2023-09-22 19:18:22 +0200

    moved progress spinner from LSM

--- src/app/shared/components/spinner/spinner.component.spec.ts
+++ src/app/shared/components/spinner/spinner.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TableComponent } from './table.component';
+import { SpinnerComponent } from './spinner.component';
 
-describe('TableComponent', () => {
-  let component: TableComponent;
-  let fixture: ComponentFixture<TableComponent>;
+describe('SpinnerComponent', () => {
+  let component: SpinnerComponent;
+  let fixture: ComponentFixture<SpinnerComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TableComponent]
+      declarations: [SpinnerComponent]
     });
-    fixture = TestBed.createComponent(TableComponent);
+    fixture = TestBed.createComponent(SpinnerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
