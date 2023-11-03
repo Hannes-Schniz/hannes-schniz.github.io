d93b679741faea6c7c484bc1f0da403c84f71dd7
Hannes <hannes.schniz@gmx.de>
2023-11-03 20:14:03 +0100

    added search function

--- src/app/shared/services/search.service.ts
+++ src/app/shared/services/search.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { project } from '../models/project';
+import { ImportService } from './import.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,7 @@ export class SearchService {
 
   public showSearch = false;
 
-  constructor() { }
+  constructor(private importService: ImportService) { }
 
   toggleSearch() {
     this.showSearch = !this.showSearch;
@@ -15,5 +17,36 @@ export class SearchService {
 
   showState() {
     return this.showSearch;
+  }
+
+  search(term:string): project[] {
+    if (term.length <= 0) {
+      return [];
+    }
+    var finds = [];
+    switch (term[0]) {
+      case "#":
+        finds = this.searchTags(term.substring(1));
+        break;
+      case "=":
+        finds = this.searchAuthors(term.substring(1));
+        break;
+      default:
+        finds = this.searchTitle(term);
+        break;
+    }
+  }
+
+  private searchTags(term: string): project[] {
+    var finds = [];
+    for (const project of this.currentProjects) {
+      for (const tag of project.tags) {
+        if (tag.toLowerCase().includes(term.toLowerCase())) {
+          finds.push(project);
+          break;
+        }
+      }
+    }
+    return finds;
   }
 }
