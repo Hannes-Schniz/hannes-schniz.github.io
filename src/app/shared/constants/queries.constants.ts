6dd085209d77d309a75751485fd04a5729593f73
Hannes <hannes.schniz@gmx.de>
2023-11-10 21:41:32 +0100

    finished help icon

--- src/app/shared/constants/queries.constants.ts
+++ src/app/shared/constants/queries.constants.ts
@@ -2,5 +2,5 @@ export const TAGQUERY = "# : ";
 export const EXPTAGQUERY = "Search for tags.";
 export const SUMQUERY = "= : ";
 export const EXPSUMQUERY = "Search for term in the summary";
-export const TITLEQUERY = "'' : "
+export const TITLEQUERY = "' ' : "
 export const EXPTITLEQUERY = "Search for term in the title";
