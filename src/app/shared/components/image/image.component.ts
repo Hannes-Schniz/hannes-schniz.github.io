58f80c36706ead0480411040de2ef27ad7acc87c
hannes.schniz <hannes.schniz@gmx.de>
2024-01-05 02:19:00 +0100

    moreeee changes (its to late)

--- src/app/shared/components/image/image.component.ts
+++ src/app/shared/components/image/image.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProjectFileModel } from '../../models/projectFile.model';
 import { ImportService } from '../../services/import.service';
 
